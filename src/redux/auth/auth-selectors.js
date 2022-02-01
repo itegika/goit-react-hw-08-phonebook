514b4e40278408638171fe660bcfbf730390b149
itegika <86248227+itegika@users.noreply.github.com>
2022-02-01 16:14:21 +0200

    Update auth-selectors.js

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,3 +1,3 @@
 export const checkIsLoggedIn = (state) => state.auth.isLoggedIn;
-export const getUserName = (state) => state.auth.user.name;
+export const getUserName = (state) => state.auth.user?.name;
 export const fetchCurrentUser = (state) => state.auth.isFetchingCurrentUser;
